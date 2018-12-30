17e06484b5cb09e635008bbafa7c8cacf0d3f38e
Olivier <lebloas.olivier@gmail.com>
2018-12-30 12:41:05 +0100

    Installation Prime React / Grid et styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import { Flex, Box } from '@rebass/grid'
 import './App.css';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+
+import {AutoComplete} from 'primereact/autocomplete';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Flex className="App">
+          <Box width={1/2} px={2}>
+              Half width
+          </Box>
+          <Box width={1/2} px={2}>
+              <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                      Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <AutoComplete/>
+                  Learn React
+              </header>
+          </Box>
+      </Flex>
     );
   }
 }
